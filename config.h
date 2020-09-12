f6b510ec60b7c8a1e272eda741143fe82d836eaf
TSwiftie <tswiftie@foxmail.com>
2020-09-12 19:22:56 +0000

    update

--- config.h
+++ config.h
@@ -88,8 +88,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_background, "-nf", col_selection, "-sb", col_selection, "-sf", col_foreground, NULL };
-static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_background, "-nf", col_selection, "-sb", col_selection, "-sf", col_foreground, NULL };
+//static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
 
