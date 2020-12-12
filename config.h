cd482c2bb191a417fa408b1247c0e8716c4fcabf
TSwiftie <tswiftie@foxmail.com>
2020-12-12 23:56:23 +0000

    change the default terminal to simple terminal

--- config.h
+++ config.h
@@ -89,7 +89,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_background, "-nf", col_selection, "-sb", col_selection, "-sf", col_foreground, NULL };
 static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "firefox", NULL };
 
 static const char *upvol[]   = { "/home/ts/scripts/vol-up.sh",  NULL };
