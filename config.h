ae991265f292f4f9d16a43d47bcdcf900b895c30
TSwiftie <tswiftie@foxmail.com>
2021-03-30 20:04:40 +0000

    update topbar

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static const unsigned int gappoh    = 10;       /* horiz outer gap between windo
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "FiraCode Nerd Font Mono:size=13" };
 static const char dmenufont[]       = "FiraCode Nerd Font Mono:size=13";
