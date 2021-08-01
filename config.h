6a6a7fe8e2fc9d7978b17a840d72bed9aa18e117
TSwiftie <tswiftie@foxmail.com>
2021-08-01 19:41:40 +0000

    change the modkey

--- config.h
+++ config.h
@@ -75,7 +75,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
